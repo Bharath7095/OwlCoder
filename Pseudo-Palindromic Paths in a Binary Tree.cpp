c058daffa3d761d17db2692b23212d63336177fc
Bharath7095 <96734418+Bharath7095@users.noreply.github.com>
2024-01-24 22:36:30 +0530

    Update Pseudo-Palindromic Paths in a Binary Tree.cpp

--- Pseudo-Palindromic Paths in a Binary Tree.cpp
+++ Pseudo-Palindromic Paths in a Binary Tree.cpp
@@ -1,20 +1,36 @@
-//leetcode potd
-int isTree(int n, int m, vector<vector<int>> &adj) {
-        // code here
-        if(n==1 and m==0) return 1;
-        vector<int>in(n,0);
-        vector<int>out(n,0);
-        vector<int>vt;
-        for(int i=0;i<m;i++)
-        {
-            in[adj[i][1]]++;
-            out[adj[i][0]]++;
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int ans;
+    void traverse(TreeNode* root,vector<int>vt)
+    {
+        if(root==NULL) return;
+        vt[root->val]++;
+        if(root->left==NULL && root->right==NULL){
+            int od=0;
+            for(int i=0;i<10;i++){
+                if(vt[i]%2!=0) od++;
+            }
+            if(od<=1) ans++;
         }
-        for(int i=0;i<n;i++)
-        {
-            vt.push_back(abs(in[i]-out[i]));
-        }
-        set<int>s(vt.begin(),vt.end());
-        if(s.size()==8) return 1;
-        return 0;
+        traverse(root->left,vt);
+        traverse(root->right,vt);
+        vt[root->val]--;
     }
+    int pseudoPalindromicPaths (TreeNode* root) {
+        ans=0;
+        vector<int>vt(10,0);
+        traverse(root,vt);
+        return ans;
+    }
+};
